e9f4ebd09b228d0ea6038d6ea8c7eef83ab4a175
BandeepGohain <144526327+BandeepGohain@users.noreply.github.com>
2025-07-30 18:26:59 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,22 @@
+public class Datatype {
+    public static void main(String[] args) {
+        byte age=25;
+        short distance=15000;
+        int population=1380000000;
+        long world_population=7800000000L;
+        float pi=3.148f;
+        double e=2.718281828459;
+        char initial='W';
+        boolean isjavaFun=true;
+
+        System.out.println("Age(byte): "+age);
+        System.out.println("Distance(short): "+distance);
+        System.out.println("Population(int): "+population);
+        System.out.println("World Population(long): "+ world_population);
+        System.out.println("PI(float): "+pi);
+        System.out.println("Eulers's number(doble): "+e);
+        System.out.println("Initial(char): "+initial);
+        System.out.println("Is java fun ?(boolean): "+isjavaFun);
+
+    }
+}
